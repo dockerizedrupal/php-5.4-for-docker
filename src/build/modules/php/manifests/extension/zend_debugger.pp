dcf90a10c5765909bf76153c9fabd92912d5da15
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-23 00:35:23 +0200

    Update

--- src/build/modules/php/manifests/extension/zend_debugger.pp
+++ src/build/modules/php/manifests/extension/zend_debugger.pp
@@ -1,13 +1,8 @@
 class php::extension::zend_debugger {
   require php
 
-  exec { 'mkdir -p /phpfarm/inst/php-5.4.33/lib/php/extensions/no-debug-non-zts-20100525':
-    path => ['/bin']
-  }
-
   file { '/phpfarm/inst/php-5.4.33/lib/php/extensions/no-debug-non-zts-20100525/ZendDebugger.so':
     ensure => present,
-    source => 'puppet:///modules/php/tmp/ZendDebugger.so',
-    require => Exec['mkdir -p /phpfarm/inst/php-5.4.33/lib/php/extensions/no-debug-non-zts-20100525']
+    source => 'puppet:///modules/php/tmp/ZendDebugger.so'
   }
 }
