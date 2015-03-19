180249262ef95bc4858add5722da8ccd6891ffed
Jürgen Viljaste <j.viljaste@gmail.com>
2015-03-19 08:15:50 +0200

    Update

--- src/php/build/modules/php/manifests/drush.pp
+++ src/php/build/modules/php/manifests/drush.pp
@@ -6,24 +6,6 @@ class php::drush {
 
   bash_exec { 'mkdir /root/.drush': }
 
-  file { '/tmp/drush-6.5.0.tar.gz':
-    ensure => present,
-    source => 'puppet:///modules/php/tmp/drush-6.5.0.tar.gz'
-  }
-
-  bash_exec { 'cd /tmp && tar xzf drush-6.5.0.tar.gz':
-    require => File['/tmp/drush-6.5.0.tar.gz']
-  }
-
-  bash_exec { 'mv /tmp/drush-6.5.0 /usr/local/src/drush6':
-    require => Bash_exec['cd /tmp && tar xzf drush-6.5.0.tar.gz']
-  }
-
-  bash_exec { 'cd /usr/local/src/drush6 && composer install':
-    timeout => 0,
-    require => Bash_exec['mv /tmp/drush-6.5.0 /usr/local/src/drush6']
-  }
-
   file { '/tmp/drush-7.0.0-alpha8.tar.gz':
     ensure => present,
     source => 'puppet:///modules/php/tmp/drush-7.0.0-alpha8.tar.gz'
--- src/php/run/modules/php/manifests/drush.pp
+++ src/php/run/modules/php/manifests/drush.pp
@@ -1,17 +1,5 @@
 class php::drush {
-  if $drupal_version == '7' {
-    file { '/usr/local/bin/drush':
-      ensure => link,
-      target => '/usr/local/src/drush6/drush'
-    }
-
-    file { '/etc/bash_completion.d/drush.complete.sh':
-      ensure => link,
-      target => '/usr/local/src/drush6/drush.complete.sh'
-    }
-  }
-
-  if $drupal_version == '8' {
+  if ($drupal_version == '7') or ($drupal_version == '8') {
     file { '/usr/local/bin/drush':
       ensure => link,
       target => '/usr/local/src/drush7/drush'
