263df4a0c6bdc3e938d70ec26d5306cbfdb61be9
Jürgen Viljaste <j.viljaste@gmail.com>
2015-06-26 10:42:22 +0300

    Update

--- src/php/build/modules/php/manifests/extension/zend_debugger.pp
+++
@@ -1,8 +0,0 @@
-class php::extension::zend_debugger {
-  require php
-
-  file { '/usr/local/src/phpfarm/inst/php-5.4.42/lib/php/extensions/no-debug-non-zts-20100525/ZendDebugger.so':
-    ensure => present,
-    source => 'puppet:///modules/php/tmp/ZendDebugger.so'
-  }
-}
---
+++ src/php/run/modules/run/manifests/coder.pp
@@ -0,0 +1,24 @@
+class run::coder {
+  if $drupal_version == '7' {
+    file { '/home/container/.drush/coder':
+      ensure => link,
+      target => '/usr/local/src/coder7'
+    }
+
+    file { '/usr/local/src/PHP_CodeSniffer_1/CodeSniffer/Standards/Drupal':
+      ensure => link,
+      target => '/usr/local/src/coder7/coder_sniffer/Drupal'
+    }
+  }
+  elsif $drupal_version == '8' {
+    file { '/home/container/.drush/coder':
+      ensure => link,
+      target => '/usr/local/src/coder8'
+    }
+
+    file { '/usr/local/src/PHP_CodeSniffer_2/CodeSniffer/Standards/Drupal':
+      ensure => link,
+      target => '/usr/local/src/coder8/coder_sniffer/Drupal'
+    }
+  }
+}
